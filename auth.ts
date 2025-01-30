7e588c49f7d99c0c09273d09f7e962a2829d2f38
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-01-29 16:32:39 -0800

    Sign up Zod schema and action

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-import { prisma } from "./db/prisma";
+import { prisma } from "@/db/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const config = {
