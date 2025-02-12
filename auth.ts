e6f9b3e727312c8ede474d59d2d6569e2b4ce987
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-02-11 16:47:30 -0800

    Session Cart Id Cookie

--- auth.ts
+++ auth.ts
@@ -6,6 +6,9 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/db/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
 export const config = {
   pages: {
     signIn: "/sign-in",
@@ -88,6 +91,29 @@ export const config = {
       }
 
       return token;
+    },
+    authorized({ request, auth }: any) {
+      // Check for session cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate new session cart id cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        response.cookies.set("sessionCartId", sessionCartId);
+
+        return response;
+      } else {
+        return true;
+      }
     },
   },
 } satisfies NextAuthConfig;
