1552d92c5f820498478df19e926a94a7ad4ce0e2
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-02-18 14:26:00 -0800

    Shipping address Zod schema and page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,4 @@
 import { clsx, type ClassValue } from "clsx";
-import { Elsie_Swash_Caps } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
