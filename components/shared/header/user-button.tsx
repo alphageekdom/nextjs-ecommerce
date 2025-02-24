740fec9602482dad50508a09de074f0ceef8dcb9
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-02-24 15:26:48 -0800

    Orders page

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -39,6 +39,7 @@ const UserButton = async () => {
             </Button>
           </div>
         </DropdownMenuTrigger>
+
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
@@ -50,6 +51,19 @@ const UserButton = async () => {
               </div>
             </div>
           </DropdownMenuLabel>
+
+          <DropdownMenuItem>
+            <Link href="/user/profile" className="w-full">
+              User Profile
+            </Link>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <Link href="/user/orders" className="w-full">
+              Order History
+            </Link>
+          </DropdownMenuItem>
+
           <DropdownMenuItem className="mb-1 p-0">
             <form action={signOutUser} className="w-full">
               <Button
