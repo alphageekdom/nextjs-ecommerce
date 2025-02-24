467341adad9c60727ebc3cc8b8d8cd1eed324091
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-02-24 15:01:54 -0800

    Get my orders action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -10,6 +10,7 @@ import { prisma } from "@/db/prisma";
 import { CartItem, PaymentResult } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
+import { PAGE_SIZE } from "../constants";
 
 // Create order and create the order item(s)
 export async function createOrder() {
@@ -186,7 +187,7 @@ export async function approvePayPalOrder(
       throw new Error("Error with PayPal payment");
     }
 
-    // UPdate order to paid
+    // Update order to paid
     await updateOrderToPaid({
       orderId,
       paymentResult: {
@@ -260,4 +261,33 @@ export async function updateOrderToPaid({
   });
 
   if (!updatedOrder) throw new Error("Order not found");
+}
+
+// Get user's orders
+export async function getMyOrders({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const session = await auth();
+
+  if (!session) throw new Error("User is not authorized");
+
+  const data = await prisma.order.findMany({
+    where: { userId: session?.user?.id },
+    orderBy: { createdAt: "desc" },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.order.count({
+    where: { userId: session?.user?.id },
+  });
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
