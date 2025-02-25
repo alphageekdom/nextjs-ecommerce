1ab5c5d5ab6e0d294ddd7d4b9dd7fc88c29c1ac7
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-02-25 14:30:13 -0800

    Update order actions for COD

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -377,3 +377,42 @@ export async function deleteOrder(id: string) {
     return { success: false, message: formatError(error) };
   }
 }
+
+// Update COD order to paid
+export async function updateOrderToPaidCOD(orderId: string) {
+  try {
+    await updateOrderToPaid({ orderId });
+
+    revalidatePath(`/order/${orderId}`);
+
+    return { success: true, message: "Order COD marked as paid" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update COD order to delivered
+export async function deliverOrder(orderId: string) {
+  try {
+    const order = await prisma.order.findFirst({ where: { id: orderId } });
+
+    if (!order) throw new Error("Order not found");
+
+    if (!order.isPaid)
+      throw new Error("Order cannot be delivered as payment is pending");
+
+    await prisma.order.update({
+      where: { id: orderId },
+      data: {
+        isDelivered: true,
+        deliveredAt: new Date(),
+      },
+    });
+
+    revalidatePath(`/order/${orderId}`);
+
+    return { success: true, message: "Order successfully marked as delivered" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
