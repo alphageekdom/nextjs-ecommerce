79c0826a942a3175c41b92c08b6a9a0e83db8b99
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-02-24 16:01:55 -0800

    Update profile action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -141,3 +141,34 @@ export async function updateUserPaymentMethod(
     return { success: false, message: formatError(error) };
   }
 }
+
+// Update the user's profile
+export async function updateProfile(user: { name: string; email: string }) {
+  try {
+    const session = await auth();
+
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        id: session?.user?.id,
+      },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name: user.name,
+      },
+    });
+
+    return {
+      success: true,
+      message: "User profile has been successfully updated.",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
