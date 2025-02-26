2f60ae4a1dbdb8f6304b541d92b2573268ff1460
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-02-25 17:31:37 -0800

    Create product page and form

---
+++ components/admin/product-form.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useToast } from "@/hooks/use-toast";
+import { productDefaultValues } from "@/lib/constants";
+import { insertProductSchema, updateProductSchema } from "@/lib/validators";
+import { Product } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "../ui/form";
+
+const ProductForm = ({
+  type,
+  product,
+  productId,
+}: {
+  type: "Create" | "Update";
+  product?: Product;
+  productId?: string;
+}) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof insertProductSchema>>({
+    resolver:
+      type === "Update"
+        ? zodResolver(updateProductSchema)
+        : zodResolver(insertProductSchema),
+    defaultValues:
+      product && type === "Update" ? product : productDefaultValues,
+  });
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8">
+        <div className="flex flex-col gap-5 md:flex-row">
+          {/* Name */}
+          {/* Slug */}
+        </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+          {/* Category */}
+          {/* Brand */}
+        </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+          {/* Price */}
+          {/* Stock */}
+        </div>
+        <div className="upload-field flex flex-col gap-5 md:flex-row">
+          {/* Images */}
+        </div>
+        <div className="upload-field">{/* Is Featured */}</div>
+        <div>{/* Description */}</div>
+        <div>{/* Submit */}</div>
+      </form>
+    </Form>
+  );
+};
+
+export default ProductForm;
