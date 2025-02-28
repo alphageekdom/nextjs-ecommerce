af7c7b48ffed5e5b768f6e36f531a03c3a906d86
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-02-27 16:46:27 -0800

    Sorting products

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -30,6 +30,8 @@ const prices = [
 
 const ratings = [4, 3, 2, 1];
 
+const sortOrders = ["newest", "lowest", "highest", "rating"];
+
 const SearchPage = async (props: SearchPageProps) => {
   const {
     q = "all",
@@ -160,7 +162,18 @@ const SearchPage = async (props: SearchPageProps) => {
             ) : null}
           </div>
 
-          <div>{/* SORT */}</div>
+          <div>
+            Sort By:{" "}
+            {sortOrders.map((s) => (
+              <Link
+                key={s}
+                className={`mx-2 ${sort == s && "font-bold"}`}
+                href={getFilterUrl({ s, baseParams })}
+              >
+                {s}
+              </Link>
+            ))}
+          </div>
         </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {products.data.length === 0 && (
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -95,7 +95,17 @@ export async function getAllProducts({
       ...priceFilter,
       ...ratingFilter,
     },
-    orderBy: { createdAt: "desc" },
+    // orderBy: { createdAt: "desc" },
+
+    orderBy:
+      sort === "lowest"
+        ? { price: "asc" }
+        : sort === "highest"
+          ? { price: "desc" }
+          : sort === "rating"
+            ? { rating: "desc" }
+            : { createdAt: "desc" },
+
     skip: (page - 1) * limit,
     take: limit,
   });
