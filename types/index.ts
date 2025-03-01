95d306406a2e1af5c46912fa2500e571f8d379a2
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2025-03-01 12:32:33 -0800

    Review prisma schema, zod, and type

--- types/index.ts
+++ types/index.ts
@@ -7,6 +7,7 @@ import {
   insertOrderItemSchema,
   insertOrderSchema,
   paymentResultSchema,
+  insertReviewSchema,
 } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -36,3 +37,9 @@ export type Order = z.infer<typeof insertOrderSchema> & {
 };
 
 export type PaymentResult = z.infer<typeof paymentResultSchema>;
+
+export type Review = z.infer<typeof insertReviewSchema> & {
+  is: string;
+  createdAt: Date;
+  user?: { name: string };
+};
